cfdd7641033e37f819b5adac64e3fdc96495e298
wenbinchen <wenbin.chen@gotinder.com>
2024-04-01 17:24:31 -0400

    use $env:GITHUB_OUTPUT

--- .github/calc_modified_strings.sh
+++ .github/calc_modified_strings.sh
@@ -14,4 +14,4 @@ do
   i=$((i+1))
 done
 concatStrings=${stringsNeedTranslation[@]}
-echo "updatedStrings=${concatStrings}" >> $env:GITHUB_OUTPUT
+echo "updatedStrings=helloworld" >> $env:GITHUB_OUTPUT
