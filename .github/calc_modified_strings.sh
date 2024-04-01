a6d38bf651d08de59d918d23720ce0f8adcd3ebd
wenbinchen <wenbin.chen@gotinder.com>
2024-04-01 14:08:21 -0400

    add string diff change

--- .github/calc_modified_strings.sh
+++ .github/calc_modified_strings.sh
@@ -4,5 +4,13 @@ echo "Starting calculating strings job"
 git status
 # switch to main branch
 git fetch origin master
-git diff --name-only origin/master HEAD > diff_result
-echo "Diff between branches: ${{ diff_result }}"
+result=$(git diff --name-only origin/master HEAD | grep '\values/strings.xml')
+fileArr=($result)
+stringsNeedTranslation=()
+i=0
+for file in ${fileArr[*]}
+do
+  stringsNeedTranslation[i]=$(git diff -U0 origin/master HEAD ${file} | grep '^\+[[:space:]]+<string name=' | grep -Ev '^(--- a/|\+\+\+ b/)')
+  i=$((i+1))
+done
+echo ${stringsNeedTranslation[@]}
