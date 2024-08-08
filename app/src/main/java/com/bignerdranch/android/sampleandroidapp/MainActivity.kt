22564c118d2fd6502db82743aef6f8d0f62bb1b2
wenbinchen <wenbin.chen@gotinder.com>
2024-08-08 16:53:43 -0400

    code clean up

--- app/src/main/java/com/bignerdranch/android/sampleandroidapp/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/sampleandroidapp/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // change to test pr merging auto delete freature
             SampleAndroidAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
