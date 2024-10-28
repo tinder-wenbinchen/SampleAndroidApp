d755358fff5d2a7c68c37699426a65f0e5729e83
wenbinchen <wenbin.chen@gotinder.com>
2024-10-28 10:50:10 -0400

    test variable passing between steps

--- .github/test_branch_delete.sh
+++ .github/test_branch_delete.sh
@@ -7,6 +7,10 @@ echo "base sha $GITHUB_BASE_SHA"
 echo "head sha $GITHUB_HEAD_SHA"
 modifiedStringsXmlFiles=$(git --no-pager diff --diff-filter=d --name-only "$GITHUB_BASE_SHA"..."$GITHUB_HEAD_SHA" | grep 'strings.xml$') || exitCode=$?
 
+OUTPUT_A_VARIABLE="15.20.0"
+# Output the milestone number to be captured by GitHub Actions
+echo "output_a_variable=$OUTPUT_A_VARIABLE" >> $GITHUB_ENV
+
 # Check if grep failed or the variable is empty
 if [ "$exitCode" -eq 1 ] || [ -z "$modifiedStringsXmlFiles" ]; then
   echo "Exiting: No strings.xml changed"
